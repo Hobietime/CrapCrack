e8adab557122f3aed3bf840d1e90a72a131cc75d
Caio <caioangelomotta@gmail.com>
2015-04-24 16:43:43 -0700

    prints only successful passwords

--- crapCrack.py
+++ crapCrack.py
@@ -63,6 +63,8 @@ if __name__ == "__main__":
 	time.sleep(1)
 	found_passwords = pool.map(multi_run_wrapper,tasks)
 
-	print(found_passwords)
+	for k in found_passwords:
+		if (k != None):
+			print k
 
 
