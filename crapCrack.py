a751f6ba54430bf64a6b660849d5eeb3e0549c5e
Caio <caioangelomotta@gmail.com>
2015-04-24 23:17:54 -0700

    removed exttra loops

--- crapCrack.py
+++ crapCrack.py
@@ -59,9 +59,9 @@ if __name__ == "__main__":
 		print(i)
 		for j in range(len(passwords)):
 			tasks.append((users[i], ctypes[i], salts[i], passwords[j], rhashes[i]))
-			for m in range(11):
+			"""for m in range(11):
 				tasks.append((users[i], ctypes[i], salts[i], passwords[j]+str(m), rhashes[i]))
-				tasks.append((users[i], ctypes[i], salts[i], passwords[j]+str(20)+str(m), rhashes[i]))
+				tasks.append((users[i], ctypes[i], salts[i], passwords[j]+str(20)+str(m), rhashes[i]))"""
 	
 	pool_size = mp.cpu_count()
 
