a2500f05c6fdfc0b1188e06c649a37b9dabe6819
Caio <caioangelomotta@gmail.com>
2015-04-24 21:30:33 -0700

    1-10 to the end of password guess

--- crapCrack.py
+++ crapCrack.py
@@ -57,7 +57,8 @@ if __name__ == "__main__":
 	for i in range(len(users)):
 		print(i)
 		for j in range(len(passwords)):
-			tasks.append((users[i], ctypes[i], salts[i], passwords[j], rhashes[i]))
+			for m in range(11):
+				tasks.append((users[i], ctypes[i], salts[i], passwords[j]+str(m), rhashes[i]))
 	
 	pool_size = mp.cpu_count()
 
