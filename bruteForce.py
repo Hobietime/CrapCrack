e1a20554d7199e5b9dd1bcde230bdbfda23058a7
Caio <caioangelomotta@gmail.com>
2015-04-27 07:20:21 -0700

    make brute force work

--- bruteForce.py
+++ bruteForce.py
@@ -2,13 +2,20 @@ import multiprocessing as mp
 import time
 from itertools import permutations
 def multi_run_wrapper(args):
-   return add(*args)
+	return deCript(*args)
 
-def add(user, ctype, salt, Ppassword, rhash):
-    print user, ctype, salt, Ppassword, rhash
-
-def start_process():
-    print 'Starting', mp.current_process().name
+def  deCript(user, ctype, salt, Ppassword, rhash):
+	if (ctype == '0'):
+		insalt = salt
+	else:
+		insalt = '${}${}$'.format(ctype, salt)
+	chash = crypt.crypt(Ppassword, insalt)
+	if (chash == rhash):
+		print "success!"
+		print user, ctype, salt, Ppassword, chash, rhash
+		return user, ctype, salt, Ppassword, chash, rhash
+	else:
+		return "fail", user, ctype, salt, Ppassword, chash, rhash
 
 if __name__ == "__main__":
 	passwordfile = open("password", 'r')
@@ -40,14 +47,16 @@ if __name__ == "__main__":
 
 	tasks = []
 	for i in range(len(users)):
-		for j in [''.join(p) for p in permutations('abcdefghijklmnopqrstuvwxyz', 6)]:
+		for j in [''.join(p) for p in permutations('1234567890', 6)]:
 			tasks.append((users[i], ctypes[i], salts[i], j, rhashes[i]))
 	
-	pool_size = mp.cpu_count()*2
 
-	pool = mp.Pool(processes=pool_size, initializer=start_process, maxtasksperchild=2, )
+	pool_size = mp.cpu_count()
+
+	pool = mp.Pool(processes=pool_size)
 	time.sleep(1)
 	found_passwords = pool.map(multi_run_wrapper,tasks)
+
 	for k in found_passwords:
 		if (k[0] == "fail"):
 			f = open("fail",'w')
